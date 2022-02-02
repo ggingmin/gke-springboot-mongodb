71ea00fb5dae80277dbd2856df358b9a21f65c2d
KYEONGMIN CHO <ggingmin@gmail.com>
2022-02-02 02:50:55 +0000

    first commit

---
+++ src/main/java/com/ggingmin/waitinglist/WaitinglistApplication.java
@@ -0,0 +1,13 @@
+package com.ggingmin.waitinglist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WaitinglistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WaitinglistApplication.class, args);
+	}
+
+}
